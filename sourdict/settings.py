06f8a9a7890ea6900f5e7a50b326b639fd9a094c
emilhsy <emilhsy@gmail.com>
2025-03-27 16:15:16 +0100

    added secret key

--- sourdict/settings.py
+++ sourdict/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oyx@*d1+=t=2fz!w9ptki2&!nsaqtq@o+ba1ow*a3=g8%k5(yx'
+# SECRET_KEY = 'django-insecure-oyx@*d1+=t=2fz!w9ptki2&!nsaqtq@o+ba1ow*a3=g8%k5(yx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
